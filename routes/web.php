3dc50a8bd193e3fe30edb667ec9dfe01e3b4b511
lgbadluck <lg.badluck@gmail.com>
2024-02-22 15:16:24 +0200

    Working ENV with send(); EmailNotification that Redirect::route to Dashboard

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group([
     'where' => ['locale' => '[a-zA-Z]{2}'],
     'middleware' => 'setlocale',
 ], function () {
+], function () {
 
     Route::get('/', function () {
         return view('welcome');
@@ -36,12 +37,11 @@ Route::group([
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        // Test for Nofication Email sending
+        // Test for Nofication Email sending 
         // author: lgbadluck
         // start
         Route::get('/send', [ProfileController::class, 'send'])->name('send');
         // end
-    });
 
         Route::get('/participant', function () {
             return view('conference.participant');
@@ -66,5 +66,8 @@ Route::group([
         Route::get('/contacts', function () {
             return view('conference.contacts');
         })->name('contacts');
+
+    });
+
+    require __DIR__ . '/auth.php';
 });
-require __DIR__ . '/auth.php';

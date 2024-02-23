3643d04c5cecb4f15d873277988919651cbe4794
Krasimira Georgieva <krassi77bg@gmail.com>
2024-02-23 09:14:54 +0200

    Fixes in routes, temp remove EmailVerification feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomEmailLink;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable //implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +43,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new CustomEmailLink);
+    }
 }

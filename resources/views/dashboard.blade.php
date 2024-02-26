3cdac5b93944e1f26290332b19707a420c0b7e09
ShingamP <s.petrovng@gmail.com>
2024-02-26 16:40:17 +0200

    Removed padding to remove empty spaces within the page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="background py-12">
+    <div class="background">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             <div class="shadow-sm sm:rounded-lg">

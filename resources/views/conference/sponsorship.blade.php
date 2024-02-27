875d593d098e96c2db43126d08e904d223580d73
MistyNaksu <tboeva@hotmail.com>
2024-02-27 10:45:39 +0200

    Adding 3 pages with Translation: Fees, Sponsorship and Accomodation

--- resources/views/conference/sponsorship.blade.php
+++ resources/views/conference/sponsorship.blade.php
@@ -1,17 +1,38 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Sponsorship') }}
         </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+    </x-slot>
+ 
+
+        <div class='flex flex-col items-center'>
+ 
+ <button></button>
+     
+        <a href="https://rndc.bg/wp-content/uploads/2023/07/Platina.pdf" target="_blank" button type="button" class=" bg-yellow-500 hover:bg-blue-700 text-white font-bold py-4 px-5 rounded my-8">{{ __('
+	Platinium Package') }}</a>
+</button>
+<div class='flex flex-col items-center'><button>
+<a href="https://rndc.bg/wp-content/uploads/2023/07/Gold.pdf" target="_blank" button type="button" class=" bg-yellow-500 hover:bg-blue-700 text-white font-bold py-4 px-9 rounded">{{ __('
+	Gold Package') }}</a>
+</button>
+</div>
+<div class='flex flex-col items-center'><button>
+<a href="https://rndc.bg/wp-content/uploads/2023/07/Silver.pdf" target="_blank" button type="button" class=" bg-yellow-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded my-8">{{ __('
+	Silver Package') }}</a>
+</button>
+</div>
+<div class='flex flex-col items-center'><button>
+<a href="https://rndc.bg/wp-content/uploads/2023/07/Bronze.pdf" target="_blank"  button type="button" class=" bg-yellow-500 hover:bg-blue-700 text-white font-bold py-4 px-7 rounded">{{ __('
+	Bronze Package') }}</a>
+
+</button>
+
+</div>
+
 </x-app-layout>
+
+

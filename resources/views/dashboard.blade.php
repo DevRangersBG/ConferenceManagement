007ae5b39697f5d253c63082ebbdeace9561b67c
SurryLeny <nvenena@gmail.com>
2024-02-27 12:31:20 +0200

    Adding BG translation empty JSON for Dashboard and Participants Pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,8 +26,7 @@
                                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-yellow-600 dark:focus:border-lime-800 focus:outline-none focus:ring-0 focus:border-lime-800 peer"
                                     placeholder=" " required />
                                 <label for="floating_first_name"
-                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First
-                                    name</label>
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ __('First name') }}</label>
                             </div>
 
                             <!--Sure Name -->
@@ -36,7 +35,7 @@
                                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-yellow-600 dark:focus:border-lime-800 focus:outline-none focus:ring-0 focus:border-lime-800 peer"
                                     placeholder=" " required />
                                 <label for="floating_sur_name"
-                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Surname</label>
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{__('Surname') }}</label>
                             </div>
 
 
@@ -46,8 +45,7 @@
                                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-yellow-600 dark:focus:border-lime-800 focus:outline-none focus:ring-0 focus:border-lime-800 peer"
                                     placeholder=" " required />
                                 <label for="floating_first_name"
-                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last
-                                    name</label>
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ __('Last name') }}</label>
                             </div>
 
                             <!--Email -->
@@ -56,8 +54,7 @@
                                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-yellow-600 dark:focus:border-lime-800 focus:outline-none focus:ring-0 focus:border-lime-800 peer"
                                     placeholder=" " required />
                                 <label for="floating_email"
-                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
-                                    address</label>
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ __('Email address') }}</label>
                             </div>
 
                             <!--Phone number -->
@@ -66,12 +63,10 @@
                                     class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-yellow-600 dark:focus:border-lime-800 focus:outline-none focus:ring-0 focus:border-lime-800 peer"
                                     placeholder=" " />
                                 <label for="floating_phone"
-                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone
-                                    number
-                                </label>
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ __('Phone number') }}</label>
                             </div>
 
-                            <!--Participants -->
+                            <!--Participants --
                             <div class="relative z-0 w-full mb-5 group">
                                 <form class="max-w-sm mx-auto">
                                     <label for="participants"
@@ -95,15 +90,12 @@
                                     class="block py-4 px-0  w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-yellow-600 dark:focus:border-lime-800 focus:outline-none focus:ring-0 focus:border-lime-800 peer"
                                     placeholder=" " />
                                 <label for="report_title"
-                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Report/article
-                                    title in Bulgarian language:
-                                </label>
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-yellow-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-lime-800 peer-focus:dark:text-lime-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{{ __('Report/article title in Bulgarian language:') }}</label>
                             </div>
 
                             <!--Attach report -->
                             <div class="relative z-0 w-full mb-5 group">
-                                <p class="mt-1 text-sm text-gray-500 dark:text-yellow-600" id="file_input_help">Attach
-                                    your report/article (for authors only, up to 2 MB) in Word</p>
+                                <p class="mt-1 text-sm text-gray-500 dark:text-yellow-600" id="file_input_help">{{ __('Attached your report/article (for authors only, up to 2 MB) in Word') }}</p>
                                 <label for="uploadFile1"
                                     class="opacity bg-white text-center rounded w-full sm:w-[360px] min-h-[160px] py-4 px-4 flex flex-col items-center justify-center cursor-pointer border-2 border-yellow-600 mx-auto font-[sans-serif] m-4">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-8 mb-6 fill-yellow-600"
@@ -115,26 +107,25 @@
                                             d="M6.707 8.707 11 4.414V17a1 1 0 0 0 2 0V4.414l4.293 4.293a1 1 0 0 0 1.414-1.414l-6-6a1 1 0 0 0-1.414 0l-6 6a1 1 0 0 0 1.414 1.414Z"
                                             data-original="#000000" />
                                     </svg>
-                                    <p class="text-yellow-600 font-semibold text-sm">Drag & Drop or <span
-                                            class="text-[#4eb2ec]">Choose file</span> to
-                                        upload</p>
+                                    <p class="text-yellow-600 font-semibold text-sm">{{ __('Drag & Drop or') }}<span
+                                            class="text-[#4eb2ec]">{{ __('Choose file') }}</span>{{ __(' to
+                                        upload')}}</p>
                                     <input type="file" id='uploadFile1' class="hidden" />
-                                    <p class="text-xs text-yellow-600 mt-2">Only Word documents are allowed.</p>
+                                    <p class="text-xs text-yellow-600 mt-2">{{ __('Only Word documents are allowed.') }}</p>
                                 </label>
 
                             </div>
 
                             <!--Abstract of the report -->
                             <div class="relative z-0 w-full mb-5 group">
-                                <p class="mt-1 text-sm text-gray-500 dark:text-yellow-600" id="file_input_help">Abstract
-                                    of the report in Bulgarian language, 100 to 150 words (mandatory for authors):</p>
+                                <p class="mt-1 text-sm text-gray-500 dark:text-yellow-600" id="file_input_help">{{ __('Abstract of the report in Bulgarian language,100 to 150 words (mandatory for authors):') }}</p>
                                 <textarea class="opacity custom-textarea"></textarea>
 
                             </div>
                             <!--Keywords -->
                             <div class="relative z-0 w-full mb-5 group">
-                                <p class="mt-1 text-sm text-gray-500 dark:text-yellow-600" id="file_input_help">Keywords
-                                    (mandatory for authors):</p>
+                                <p class="mt-1 text-sm text-gray-500 dark:text-yellow-600" id="file_input_help">{{ __('Keywords
+                                    (mandatory for authors):') }}</p>
                                 <textarea class=" opacity custom-textarea"></textarea>
 
                             </div>
@@ -143,13 +134,13 @@
                             <div class="relative z-0 w-full mb-5 group">
                                 <form class="max-w-sm mx-auto">
                                     <label for="participants"
-                                        class="block py-2.5 px-0 w-full text-sm text-gray-500 dark:text-yellow-600">Thematic
-                                        areas you wish to participate in:</label>
+                                        class="block py-2.5 px-0 w-full text-sm text-gray-500 dark:text-yellow-600">{{ __('Thematic
+                                        areas you wish to participate in:') }}</label>
                                     <select id="underline_select"
                                         class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-yellow-600 dark:border-yellow-600 focus:outline-none focus:ring-0 focus:border-lime-800 peer">
-                                        <option selected>Policies, strategies, leadership</option>
-                                        <option>Research, analysis, forecasts</option>
-                                        <option>Operational compatibility</option>
+                                        <option selected>{{ _('Policies, strategies, leadership') }}</option>
+                                        <option>{{ __('Research, analysis, forecasts') }}</option>
+                                        <option>{{ __('Operational compatibility') }}</option>
                                     </select>
                                 </form>
                             </div>
@@ -158,12 +149,11 @@
                             <div class="relative z-0 w-full mb-5 group">
                                 <form class="max-w-sm mx-auto">
                                     <label for="participants"
-                                        class="block py-2.5 px-0 w-full text-sm text-gray-500 dark:text-yellow-600">Do
-                                        you wish to receive a certificate of participation in the conference?:</label>
+                                        class="block py-2.5 px-0 w-full text-sm text-gray-500 dark:text-yellow-600">{{ __('Do you wish to receive a certificate of participation in the conference?:') }}</label>
                                     <select id="underline_select"
                                         class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-yellow-600 dark:border-yellow-600 focus:outline-none focus:ring-0 focus:border-lime-800 peer">
-                                        <option selected>Yes</option>
-                                        <option>No</option>
+                                        <option selected>{{ __('Yes') }}</option>
+                                        <option>{{__('No') }}</option>
                                     </select>
                                 </form>
                             </div>
@@ -174,7 +164,7 @@
                                     class="relative  inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium font-style: italic text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-lime-800 group-hover:from-cyan-500 group-hover:to-lime-800 hover:text-yellow-100 dark:text-blue-200 focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                                     <span
                                         class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-yellow-600 rounded-md group-hover:bg-opacity-0">
-                                        SUBMIT
+                                        {{__('SUBMIT')}}
                                     </span>
                                 </button>
                             </div>
@@ -196,8 +186,8 @@
                                     <img src="https://img.icons8.com/fluent/30/000000/youtube.png" />
                                 </a>
                             </div>
-                                <p class="text-center text-gray-700 font-medium">Sofia, 1504
-                                    Boulevard "Evlogi and Hristo Georgievi" No. 82.
+                                <p class="text-center text-gray-700 font-medium">{{ __('Sofia, 1504
+                                    Boulevard "Evlogi and Hristo Georgievi" No. 82.')}}
                                     <br>+359 2/ 92 26 550<br>
                                     <br>rnda@armf.bg<br></p>
                         </footer>

15dba0753483a11de58ba27a9e9b4d4337c043a4
tommycws2018 <tommycws2018@gmail.com>
2024-03-09 20:37:15 +0200

    fixed position in contacts

--- resources/views/conference/contacts.blade.php
+++ resources/views/conference/contacts.blade.php
@@ -5,17 +5,17 @@
         </h2>
     </x-slot>
 
-    <div class="py-12 " style="background: url('/images/contacts_background.svg') center; background-size: cover;height: 80vh;">
+    <div class="py-48 " style="background: url('/images/contacts_background.svg') center; background-size: cover; height: 80vh;">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="opacity bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+            <div class="opacity py-2 max-w-md mx-auto mt-80 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-2 text-gray-900">
+
                     <p class="text-center my-4"><b>{{ __('Contacts with the organizing committee:') }}</b></p>
-                    <p class="text-center"><b>{{ __('Sofia 1504') }}</b></p>
-                    <p class="text-center"><b>{{ __('82 Evlogi & Hristo Georgievi Blvd.') }}</b></p>
-                    <p class="text-center"><b>{{ __('Rakovski National Defence College') }}</b></p>
-                    <p class="text-center"><b><a href="mailto:ncva@rndc.bg" class="text-yellow-600
-                    dark:text-blue-500
-                    hover:text-green-800"> ncva@rndc.bg</a></b></p>
+                    <p class="text-center">{{ __('1504 Sofia') }}</p>
+                    <p class="text-center">{{ __('82 Evlogi & Hristo Georgievi Blvd.') }}</p>
+                    <p class="text-center">{{ __('Rakovski National Defence College') }}</p>
+                    <p class="text-center"><b><a href="mailto:ncva@rndc.bg" class="text-yellow-600 hover:text-green-800"> ncva@rndc.bg</a></b></p>
+
                 </div>
             </div>
         </div>
